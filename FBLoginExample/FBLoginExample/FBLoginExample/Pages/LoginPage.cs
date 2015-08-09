3322176b9e155a88675d136a61420fa9045cd801
codercampos <carlos.campos@kadevjo.com>
2015-08-09 00:02:52 -0600

    Improving UI on LoginPage

--- FBLoginExample/FBLoginExample/FBLoginExample/Pages/LoginPage.cs
+++ FBLoginExample/FBLoginExample/FBLoginExample/Pages/LoginPage.cs
@@ -15,8 +15,32 @@ namespace FBLoginExample.Pages
                 VerticalOptions = LayoutOptions.FillAndExpand,
                 Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0)
             };
+            Label lblTitle = new Label
+            {
+                Text = "Facebook LoginButton Example",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                XAlign = TextAlignment.Center,
+                TextColor = Color.Black
+            };
+            Label lblExplanation = new Label
+            {
+                Text = "This will show you how the Facebook LoginButton look like using Xamarin Forms CustomRenderer",
+                FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                XAlign = TextAlignment.Center,
+                TextColor = Color.Black
+            };
             FacebookButton fbButton = new FacebookButton();
+            fbButton.HeightRequest = 60;
+            fbButton.VerticalOptions = LayoutOptions.End;
+            //Add your event handler for the OnLogin to operate with the Facebook credentials comming from SDK
             fbButton.OnLogin += LoginWithFacebook;
+
+            //Adding views to layout
+            layout.Children.Add(lblTitle);
+            layout.Children.Add(lblExplanation);
             layout.Children.Add(fbButton);
             this.Content = layout;
         }
@@ -37,7 +61,7 @@ namespace FBLoginExample.Pages
 
             if (success)
             {
-                var message = string.Format("You have succesfully access to your facebook account. Data returned:\nUserId: {0}\nAccess Token: {1}\nExpiration Date: {3}", 
+                var message = string.Format("You have succesfully access to your facebook account. Data returned:\n\nUserId: {0}\n\nAccess Token: {1}\n\nExpiration Date: {2}", 
                     e.UserId, e.AccessToken, e.TokenExpiration);
                 await DisplayAlert("Success", message, "Ok");
             }
